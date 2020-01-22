44a6b46dc57fcc3d793e9d6805b4da4bd0ff3c3e
Xilin Chen <xilinc@med.umich.edu>
2020-01-22 10:24:46 -0500

    save milestone csv to rdata

---
+++ code/save_milestone_csv_to_rdata.R
@@ -0,0 +1,8 @@
+# this document is initial exploration for the milestone data
+
+library(tidyverse)
+
+milestone = read_csv("/Volumes/George_Surgeon_Projects/ACGME_milestone/data/Data-UFA04037 ACGME George Mich DUA PE 4Dec2019_ESHsigned.csv",
+                     col_types = cols(.default = "c"))   # read all as charactor
+
+save(milestone, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/data/milestone_01_2020.rdata")
