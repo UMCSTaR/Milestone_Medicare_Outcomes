0050540fc2f65857a327a6d9b01e96490e32c4de
Xilin Chen <xilinc@med.umich.edu>
2020-02-03 11:20:21 -0500

    milestone csv to rdata

--- code/save_milestone_csv_to_rdata.R
+++ code/save_milestone_csv_to_rdata.R
@@ -5,12 +5,11 @@ library(tidyverse)
 milestone = read_csv("/Volumes/George_Surgeon_Projects/ACGME_milestone/data/Data-UFA04037 ACGME George Mich DUA PE 4Dec2019_ESHsigned.csv",
                      col_types = cols(.default = "c"))   # read all as charactor
 
-save(milestone, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/data/milestone_01_2020.rdata")
-
-
 # date
-milestone %>% 
-  select(contains("Date")) %>% 
+milestone = milestone %>% 
   mutate(Birth_year = year(dmy(BirthDate)),
          Degree_year = year(dmy(DegreeDate)),
          complete_year = year(mdy(ExpectedCompletionDate)))
+
+save(milestone, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/data/milestone_01_2020.rdata")
+
