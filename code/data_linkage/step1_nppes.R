e7484a3ce90f5641dcac08b33e90ae28cbbbf97e
Xilin Chen <xilinc@med.umich.edu>
2020-02-10 11:39:20 -0500

    nppes link to milestone

---
+++ code/data_linkage/step1_nppes.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+
+load("/Volumes/George_Surgeon_Projects/ACGME_milestone/milestone_person_info.rdata")
+# nppes
+load("/Volumes/George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var.rdata")
+
+
+npidata_pfile_2020_selected_var = npidata_pfile_2020_selected_var %>% 
+  mutate(NPI = as.character(NPI))
+
+milestone_person_nppes = milestone_person_info %>% 
+  left_join(npidata_pfile_2020_selected_var)
+
+# check
+milestone_person_nppes %>% 
+  filter(!is.na(NPI), is.na(first_name)) %>% 
+  glimpse()
+# every npi from milestone is connect to NPPES, some are deactived or no NPI
+
+save(milestone_person_nppes, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/milestone_person_nppes.rdata")
+
+glimpse(milestone_person_nppes)
+
+# entity type
+milestone_person_nppes %>% 
+  filter(!is.na(NPI)) %>% 
+  count(`Entity Type Code`)
+
+# `Entity Type Code`     n
+# <dbl> <int>
+#   1                  1  3091
+# 2                  2     3
+# 3                 NA     2
+
+
+
+# Reverse Check with NPPES ---------
+load("/Volumes/George_Surgeon_Projects/ACGME_milestone/milestone_person_nppes.rdata")
+milestone_person_nppes = milestone_person_nppes%>% 
+  rename(npi_ms = NPI)
+
+load("/Volumes/George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var_md_do.rdata")
+
+npidata_pfile_2020_selected_var_md_do = npidata_pfile_2020_selected_var_md_do %>% 
+  mutate(md_do_nppes = 1)
+
+milestone_person_nppes_v1 = milestone_person_nppes %>% 
+  left_join(npidata_pfile_2020_selected_var_md_do, 
+            by = c("first_name", "last_name", "middle_name"))
+
+# multiple match -----
+multi_nppes = milestone_person_nppes_v1 %>% 
+  filter(!is.na(NPI)) %>% 
+  add_count(PersonID) %>% 
+  filter(n>1)
+
+n_distinct(multi_nppes$PersonID)  #6
+
+#  non-match -------
+non_mat_nppes = milestone_person_nppes_v1 %>% 
+  filter(is.na(NPI)) 
+
+n_distinct(non_mat_nppes$PersonID)  #1892
+
+# QA: unique macthed to check against milestone ------
+milestone_person_nppes_v1 %>% 
+  filter(!is.na(NPI)) %>% 
+  add_count(PersonID) %>% 
+  filter(n==1) %>% 
+  mutate(nppes_miles_npi = ifelse(npi_ms == NPI, "same", "diff")) %>% 
+  tidyext::cat_by(nppes_miles_npi)
+
+# 0.4% miss
+
+npi_diff = milestone_person_nppes_v1 %>% 
+  filter(!is.na(NPI)) %>% 
+  add_count(PersonID) %>% 
+  filter(n==1) %>% 
+  mutate(nppes_miles_npi = ifelse(npi_ms == NPI, "same", "diff")) %>% 
+  filter(nppes_miles_npi == "diff") %>% 
+  select(PersonID, npi_ms, NPI, contains("name"))
+
+npi_ms_diff = unique(npi_diff$npi_ms)
+
+load("/Volumes/George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var.rdata")
+qa = npidata_pfile_2020_selected_var %>% 
+  mutate(NPI = as.character(NPI)) %>% 
+  filter(NPI %in% npi_ms_diff) %>% 
+  select(NPI, `Provider Credential Text`)
+
+# based on qa, the differences are from the `Provider Credential Text`, NPI from Milestones are non MD or DO.
