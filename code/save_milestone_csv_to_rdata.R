6ebf8451d04a29631dcc03f4c9b5ed38136ec0f8
Xilin Chen <xilinc@med.umich.edu>
2020-02-11 10:09:42 -0500

    update new milestone data

--- code/save_milestone_csv_to_rdata.R
+++ code/save_milestone_csv_to_rdata.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(lubridate)
 
+# this take 2 min
 milestone = read_csv("/Volumes/George_Surgeon_Projects/ACGME_milestone/original/ACGME Milestones Data Update 19-UFA04037 2020.02.05.csv",
                      col_types = cols(.default = "c"))   # read all as charactor
 
@@ -10,11 +11,8 @@ milestone = read_csv("/Volumes/George_Surgeon_Projects/ACGME_milestone/original/
 milestone = milestone %>% 
   mutate(Birth_date = dmy(BirthDate),
          Degree_date = dmy(DegreeDate),
-         complete_date = mdy(ExpectedCompletionDate))
-
-# ssn
-milestone = milestone%>% 
-  mutate(ssn = str_extract(Last4SSN, "\\d+")) 
+         complete_date = mdy(ExpectedCompletionDate),
+         birth_year = lubridate::year(Birth_date))
 
 save(milestone, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/original/milestone_02_2020.rdata")
 
