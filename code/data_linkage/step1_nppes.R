31bf91351add16bcd9cf8a0a821332cdc1aa910f
Xilin Chen <xilinc@med.umich.edu>
2020-02-12 16:10:48 -0500

    found a bug due to missing credential

--- code/data_linkage/step1_nppes.R
+++ code/data_linkage/step1_nppes.R
@@ -44,14 +44,14 @@ milestone_person_nppes = milestone_person_nppes %>%
   rename(npi_ms = NPI) # renanme milestone NPI to diff with NPPES NPI
 
 # load NPPES with DO or MD 
-load("/Volumes/George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var_md_do.rdata")
+load("/Volumes/George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var_md_do_na.rdata")
 
-npidata_pfile_2020_selected_var_md_do = npidata_pfile_2020_selected_var_md_do %>% 
+npidata_pfile_2020_selected_var_md_do_na = npidata_pfile_2020_selected_var_md_do_na %>% 
   mutate(md_do_nppes = 1) # add flag to indicate data from nppes
 
 # milestone names left join with nppes names
 milestone_person_nppes_match = milestone_person_nppes %>% 
-  left_join(npidata_pfile_2020_selected_var_md_do, 
+  left_join(npidata_pfile_2020_selected_var_md_do_na, 
             by = c("first_name", "last_name", "middle_name"))
 
 # one milestone with multiple match NPPES -----
@@ -61,14 +61,14 @@ multi_nppes = milestone_person_nppes_match %>%
   filter(n>1) %>% 
   select(-n)
 
-n_distinct(multi_nppes$PersonID)  #144
+n_distinct(multi_nppes$PersonID)  #223
 save(multi_nppes, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/step1_nppes/multi_nppes.rdata")
 
 #  non-match -------
 non_matched_nppes = milestone_person_nppes_match %>% 
-  filter(is.na(NPI)) 
+  filter(is.na(NPI), !is.na(npi_ms)) 
 
-n_distinct(non_matched_nppes$PersonID)  #1892
+n_distinct(non_matched_nppes$PersonID)  #35
 save(non_matched_nppes, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/step1_nppes/non_match.rdata")
 
 # unique macthed to check against milestone ------
@@ -80,11 +80,11 @@ matched_nppes = milestone_person_nppes_match %>%
 
 save(matched_nppes, file = "/Volumes/George_Surgeon_Projects/ACGME_milestone/step1_nppes/matched_nppes.rdata")
 
-# QA
+# QA --------
 matched_nppes %>% 
   mutate(nppes_miles_npi = ifelse(npi_ms == NPI, "same", "diff")) %>% 
   tidyext::cat_by(nppes_miles_npi)
-# 0.4% miss
+# 0.1% miss
 
 npi_diff = milestone_person_nppes_match %>% 
   filter(!is.na(NPI)) %>% 
