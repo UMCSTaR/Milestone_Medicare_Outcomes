12b74c7e8a2308d5b1bee55ea1e6428e2e1c00a3
Xilin Chen <xilinc@med.umich.edu>
2020-02-17 14:58:50 -0500

    select medicare cases

--- code/analysis/link_with_medicare.R
+++ code/analysis/link_with_medicare.R
@@ -13,7 +13,7 @@ load("/Volumes/George_Surgeon_Projects/medicare_data/xilin_analytic_file/add_cmb
 
 
 # match by NPI process -------
-## unique match -----
+## 1. unique match -----
 n_distinct(analytic_data$id_physician_npi) # n 37988
 n_distinct(milestone_person$npi.linked)    # n 4628
 
@@ -25,26 +25,29 @@ n_distinct(milestone_medicare$id_physician_npi) # 676
 milestone_medicare %>% 
   cat_by(facility_clm_yr)
 
-# multiple matches ----------
+milestone_medicare_unique = milestone_medicare %>% 
+  distinct(PersonID, id_physician_npi) %>% 
+  glimpse()
+
+# 2. multiple matches ----------
+# 2.1 multi match by NPEES and can't decide by yog or yob ------
 milestone_person_m = milestone_person %>% 
   filter(is.na(npi.linked))
 
 multi = milestone_nppes_ama_abs_15_18 %>% 
   select(PersonID, NPI.ms, NPI.nppes) %>% 
   gather("npi_version", "npi", -PersonID) %>% 
-  filter(PersonID %in% milestone_person_m$PersonID)
-
-multi = multi %>% 
+  filter(PersonID %in% milestone_person_m$PersonID) %>% 
   filter(!is.na(npi)) 
 
+# check
 n_distinct(multi$PersonID)  # 15
-
 n_distinct(milestone_multi_npi$PersonID) # 4642
 
 milestone_medicare_muti = analytic_data %>% 
   inner_join(multi, by = c("id_physician_npi" = "npi"))
 
-milestone_medicare_muti %>% 
+milestone_match_with_medicare_person = milestone_medicare_muti %>% 
   distinct(PersonID, id_physician_npi)
 
 # PersonID id_physician_npi
@@ -54,9 +57,55 @@ milestone_medicare_muti %>%
 milestone_medicare_muti %>% 
   count(facility_clm_yr)
 
-# no need to do the multiple match 15 people, since only 2 got matched with 15 cases
-# all the way back to 2007
 
-# AMA and ABS with no matches
+# 2.2 AMA and ABS with no matches, by names ----------
 load("/Volumes/George_Surgeon_Projects/ACGME_milestone/linkage/de_name_data/no_match_ama.rdata")
 load("/Volumes/George_Surgeon_Projects/ACGME_milestone/linkage/de_name_data/no_match_abs.rdata")
+
+# ama
+npi_ama = no_match_ama %>% 
+  distinct(PersonID, npi) %>% 
+  rename(npi.ama = npi)
+
+ama_medicare_person = analytic_data %>% 
+  inner_join(npi_ama, by = c("id_physician_npi" = "npi.ama")) %>% 
+  distinct(PersonID, id_physician_npi) %>% 
+  glimpse()
+# 7
+
+# abs 
+npi_abs = no_match_abs %>% 
+  distinct(PersonID, npi.abs) 
+
+abs_medicare_person = analytic_data %>% 
+  inner_join(npi_abs, by = c("id_physician_npi" = "npi.abs")) %>% 
+  distinct(PersonID, id_physician_npi) %>% 
+  glimpse()
+# 3
+
+# 3. combine unique and multiple --------
+
+milestone_medicare_person = rbind(milestone_medicare_unique,
+milestone_match_with_medicare_person,
+ama_medicare_person,
+abs_medicare_person) %>% 
+  distinct()
+
+# check dup for personID and NPI
+milestone_medicare_person %>% 
+  add_count(PersonID) %>% 
+  filter(n>1)
+
+milestone_medicare_person %>% 
+  add_count(id_physician_npi) %>% 
+  filter(n>1)
+
+# select medicare cases
+milestone_medicare = analytic_data %>% 
+  inner_join(milestone_medicare_person, by = "id_physician_npi")
+
+n_distinct(milestone_medicare$id_physician_npi)
+
+
+save(milestone_medicare, file = "/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/milestone_medicare.rdata")
+
---
+++ code/functions/prep_medicare_data.R
@@ -0,0 +1,134 @@
+prep_data_for_model <- function(
+  data, 
+  standardize = vars(age_at_admit, AHRQ_score),
+  collapse_race = TRUE,
+  proc = NULL
+) {
+  
+  # package requirements ----------------------------------------------------
+  if(!requireNamespace('tidyverse') | !requireNamespace("tidyext"))
+    stop('tiydverse and tidyext required. devtools::install_github(m-clark/tidyext)')
+  
+  
+  # # potential filters -------------------------------------------------------
+  # # cohort selection (may already be done)
+  # data = data %>% 
+  #   filter(id_physician_npi != "" &       # drop physician NPI
+  #            !is.na(val_yr_practice))     # drop year after graduation NA
+  
+  
+  # make a procedure category for multiple procedure 
+  data = data %>% 
+    mutate(e_proc_grp_lbl = ifelse(flg_multi_cpt == 1, "multi_procedures", e_proc_grp_lbl))
+  
+  
+  # deal with procedure filter and 
+  if (!is.null(proc)) {
+    data = data %>% 
+      filter(e_proc_grp_lbl %in% proc)
+  } else {
+    # onehot proc group: 
+    # 1 Appendectomy              
+    # 2 Cholecystectomy       
+    # 3 Groin Hernia Repair 
+    # 4 multi_procedures      
+    # 5 Partial Colectomy     
+    # 6 Ventral Hernia Repair 
+    
+    data = data %>% 
+      tidyext::onehot(var = 'e_proc_grp_lbl', keep.original = T) %>% 
+      rename(
+        proc_Appendectomy          = e_proc_grp_lbl_Appendectomy, 
+        proc_Partial_Colectomy     = e_proc_grp_lbl_Partial.Colectomy,
+        proc_Cholecystectomy       = e_proc_grp_lbl_Cholecystectomy,
+        proc_Groin_Hernia_Repair   = e_proc_grp_lbl_Groin.Hernia.Repair,
+        proc_Ventral_Hernia_Repair = e_proc_grp_lbl_Ventral.Hernia.Repair,
+        proc_multi_procedures      = e_proc_grp_lbl_multi_procedures
+      )
+    
+  }
+  
+  
+  # standardize numeric -----------------------------------------------------
+  
+  data = data %>% 
+    mutate_at(standardize, function(x) scale(x)[,1]) 
+  
+  
+  # onehot categorical ------------------------------------------------------
+  
+  # race; wbho = 1234
+  data = data %>% 
+    onehot(var = 'e_race_wbho', keep.original = T) %>% 
+    rename(
+      race_white = e_race_wbho_1,
+      race_black = e_race_wbho_2
+    )
+  
+  if (collapse_race) {
+    data = data %>% 
+      mutate(
+        race_hisp_other = e_race_wbho_3 | e_race_wbho_4
+      )
+  } else {
+    data = data %>% 
+      rename(
+        race_hispanic = e_race_wbho_3,
+        race_other    = e_race_wbho_4
+      )
+  }
+  
+  # e_admit_type 1- emergency 2 urgent 3 elective 4 other 9 UK/missing
+  # Given that 'other' is indistinguishable from unknown, which was lumped with
+  # missing, other will be given NA as well.  It has very few values anyway.  This was checked for appropriate NA
+  
+  data = data %>% 
+    mutate(e_admit_type = ifelse(e_admit_type == 4 | e_admit_type == 9, 
+                                 NA, 
+                                 e_admit_type)) %>% 
+    mutate(flg_admit_emerg = ifelse(e_admit_type == 3, 0, 1))
+  
+  
+  
+  # dichotomize Hospital Bed size
+  data = data %>% 
+    mutate(hosp_beds_2grp = ifelse(e_hosp_beds_4grp == 1 | e_hosp_beds_4grp == 2, 
+                                   0, 
+                                   1),
+           hosp_beds_2grp = ifelse(is.na(e_hosp_beds_4grp),
+                                   NA, 
+                                   hosp_beds_2grp))
+  
+  #dichotomize SES
+  data = data %>% 
+    mutate(ses_2grp = ifelse(e_ses_5grp == 1 | e_ses_5grp == 2, 
+                             0, 
+                             1),
+           ses_2grp = ifelse(is.na(e_ses_5grp) , 
+                             NA, 
+                             ses_2grp))
+  
+  # convert logical to integers ---------------------------------------------
+  data = data %>% 
+    mutate_at(vars(matches('^race|^proc|^flg_admit_emerg')), as.integer)
+  
+  
+  
+  # misc other --------------------------------------------------------------
+  
+  # Add to this as necessary
+  # quintiles actually aren't quintiles because there is only info for 1:5 and
+  # yet there is a zero category.  Presumably missing.
+  data = data %>% 
+    mutate(
+      id_physician_npi = factor(id_physician_npi),
+      facility_prvnumgrp = factor(facility_prvnumgrp),
+      e_ses_5grp = ifelse(e_ses_5grp == 0, NA, e_ses_5grp),
+      facility_clm_yr = facility_clm_yr - 2007
+    ) 
+  
+  
+  # return ------------------------------------------------------------------
+  
+  as_tibble(data)
+}
