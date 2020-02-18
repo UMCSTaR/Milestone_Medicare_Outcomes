b0aaf8dc94a4e67cb16d162e50360e08407e82fa
Xilin Chen <xilinc@med.umich.edu>
2020-02-18 14:38:01 -0500

    milestone data combine

--- code/milestone_data_EDA/milestone_data.R
+++ code/milestone_data_EDA/milestone_data.R
@@ -1,23 +1,42 @@
 library(tidyverse)
+library(tidyext)
 
+# load data -----
+# evaluations
 load("/Volumes/George_Surgeon_Projects/ACGME_milestone/original/milestone_2015.rdata")
-
-glimpse(milestone_15_all)
-
-milestone_15_all %>% 
-  distinct(AcademicYear, PersonID) %>% 
-  count(AcademicYear)
-
 load("/Volumes/George_Surgeon_Projects/ACGME_milestone/original/milestone_02_2020.rdata")
+# people from 
+milestone_person = read_csv("/Volumes/George_Surgeon_Projects/ACGME_milestone/linkage/de_name_data/milestone_linked_npi.csv") %>% 
+  mutate_if(is.numeric, as.character)
 
-glimpse(milestone)
+milestone_678 = milestone %>% 
+  select(PersonID, ScheduleWindowDescription, AcademicYear, SurveyName, SponsorID, residentyear, QuestionID:ResponseText) %>% 
+  rename(eval_peroid = ScheduleWindowDescription) %>% 
+  glimpse()
 
-milestone %>% 
-  distinct(AcademicYear, PersonID) %>% 
-  count(PersonID) %>% 
-  count(n)
+# check
+milestone_678 %>% 
+  cat_by(eval_peroid)
 
-milestone_15_all %>% 
-  distinct(PersonID) %>% 
-  mutate(PersonID = as.character(PersonID)) %>% 
-  inner_join(milestone)
+# combine all year milestone ------
+milestone_5 = milestone_15_all %>% 
+  select(PersonID, ScheduleWindowDescription, AcademicYear, SurveyName, SponsorID, residentyear, QuestionID:ResponseText) %>% 
+  rename(eval_peroid = ScheduleWindowDescription) %>% 
+  glimpse()
+
+milestone = rbind(milestone_678, milestone_5) %>% 
+  filter(PersonID %in% milestone_person$PersonID)
+
+n_distinct(milestone$PersonID)  #4720
+
+
+# Final year milestone -------
+milestone_final_year = milestone %>% 
+  filter(residentyear == 5)
+
+# check
+n_distinct(milestone_final_year$PersonID)  # 4718
+
+
+save(milestone_final_year, file = "/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/milestone_final_year.rdata")
+
