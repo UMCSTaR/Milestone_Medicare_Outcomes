36f26236655ae8c833de49151b73ff7d39b196c8
Xilin Chen <xilinc@med.umich.edu>
2020-02-25 15:22:30 -0500

    add operative and professionalism

--- code/analysis/step3_milestone_prepare.R
+++ code/analysis/step3_milestone_prepare.R
@@ -7,12 +7,11 @@ load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/milestone_medicare_
 load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/milestone_final_year.rdata")
 
 
-
 # select milstone records for medicare physician -------
 milestone_person_in_medicare = milestone_final_year %>% 
   filter(PersonID %in% milestone_medicare_pc$PersonID)
 
-milestone_end_person_in_medicare  = milestone_person_in_medicare%>% 
+milestone_end_person_in_medicare  = milestone_person_in_medicare %>% 
   filter(grepl("Year-End",eval_peroid))
 
 # check
@@ -28,6 +27,7 @@ no_end_eval_person = milestone_person_in_medicare %>%
 
 range(milestone_end_person_in_medicare$IntResponseValue)
 
+# mean milestone ratings --------
 person_values = milestone_end_person_in_medicare %>% 
   group_by(PersonID) %>% 
   mutate(IntResponseValue = as.numeric(IntResponseValue),
@@ -36,12 +36,39 @@ person_values = milestone_end_person_in_medicare %>%
   ungroup() %>% 
   mutate(IntResponseValue_mean_scale = as.numeric(scale(IntResponseValue_mean))) %>% 
   glimpse()
-  
+
+save(person_values, file = "data/person_in_medicare_and_milestone.rdata")
   
 ggplot(data = person_values) +
   geom_histogram(aes(x = IntResponseValue_mean_scale)) +
   theme_classic() +
   ggtitle("Mean Eval Rating for End-year eval among surgeons in Medicare")
+
+# mean operative milestone ratings (PC3, MK2, ICS3)------
+milestone_end_person_in_medicare %>% 
+  count(QuestionKey)
+
+operative_rating = milestone_end_person_in_medicare %>% 
+  filter(QuestionKey %in% c("comp1_PC_Q3", "comp2_MK_Q2", "comp6_ICS_Q3")) %>% 
+  group_by(PersonID) %>% 
+  mutate(operative_rating_mean = mean(as.numeric(IntResponseValue), na.rm = T)) %>% 
+  distinct(PersonID, operative_rating_mean) %>% 
+  ungroup() %>% 
+  glimpse()
+
+# mean professionalism
+prof_rating = milestone_end_person_in_medicare %>% 
+  filter(ReportCategory == "Professionalism") %>% 
+  group_by(PersonID) %>% 
+  mutate(prof_rating_mean = mean(as.numeric(IntResponseValue), na.rm = T)) %>% 
+  distinct(PersonID, prof_rating_mean) %>% 
+  ungroup() %>% 
+  glimpse()
+
+# person value all
+person_values = person_values %>% 
+  left_join(prof_rating) %>% 
+  left_join(operative_rating)
 
 # attach score to the medicare -----
 # (inner join loose 7 people who don't have end-year eval)
