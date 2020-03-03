9fcb3d58984192642e67aab730d2c6e05854ed7d
Xilin Chen <xilinc@med.umich.edu>
2020-03-03 16:36:59 -0500

    all or plots

--- code/visual/odds_ratio_plot.R
+++ code/visual/odds_ratio_plot.R
@@ -23,7 +23,7 @@ load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/low_rating_re
 less_than_7 = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
   filter(term == "ever_less_7_rating")
 
-# Odds ratio for all outcomes by predictors
+# Odds ratio for all outcomes by predictors for partical colectomy-----------
 
 rbind(mean_rating, prof_rating, operative_mean, less_than_7) %>% 
   mutate(OR = exp(value),
@@ -50,16 +50,35 @@ rbind(mean_rating, prof_rating, operative_mean, less_than_7) %>%
 ggsave("images/or_bar.png")
 
 
-# correlation betweeen ratings -------
+# odds ratio plot for all procedures, outcomes and milestones -------
 
-library(PerformanceAnalytics)
-library(data.table)
+load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/all_out_proc_rating.rdata")
 
-milestone_medicare_pc = data.table(milestone_medicare_pc)
+primaries = c("IntResponseValue_mean", "prof_rating_mean", "operative_rating_mean",
+              "ever_less_7_rating")
 
-load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/milestone_medicare_pc.rdata")
+map_df(results, extract_fixed_effects, .id = "outcome") %>% 
+  filter(term %in% primaries) %>% 
+  mutate(OR = exp(value),
+         OR_lower = exp(lower_2.5),
+         OR_upper = exp(upper_97.5)
+  ) %>% 
+  filter(se<1) %>% 
+  ggplot(aes(y = OR, x = reorder(outcome, OR))) +
+  geom_point() +
+  geom_vline(xintercept = 0, color = "firebrick4", alpha = 1/10) +
+  geom_hline(yintercept = 1, alpha = 0.25) +
+  geom_pointrange(aes(ymin = OR_lower,
+                      ymax = OR_upper),
+                  # .width = .95,
+                  size = 3/4, color = "#990024") +
+  lims(y = c(0.5,2)) +
+  coord_flip() +
+  theme(panel.grid   = element_blank(),
+        panel.grid.major.y = element_line(color = alpha("firebrick4", 1/4), linetype = 3),
+        axis.text.y  = element_text(hjust = 0),
+        axis.ticks.y = element_blank()) +
+  xlab("Patient Outcomes") 
 
-my_data <- milestone_medicare_pc[, .(IntResponseValue_mean, prof_rating_mean, operative_rating_mean)]
-
-chart.Correlation(my_data, histogram=TRUE) 
+ggsave("code/visual/or_all.png")
 
