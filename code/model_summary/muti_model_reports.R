cf0986680e73f364c268a4c2be0aaabcf339ef6e
Xilin Chen <xilinc@med.umich.edu>
2020-07-17 10:35:07 -0400

    model summary reports

--- code/model_summary/muti_model_reports.R
+++ code/model_summary/muti_model_reports.R
@@ -1,22 +1,46 @@
 library(knitr)
 library(readr)
-library(dplyr)
+library(tidyverse)
 
-filelist <- list.files("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/pc/")
+load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/pc/rating_model.rdata")
 
-# loop through the file list to read in data and clean it up
+# severe complication ------
+severe_cmp = names(results) %>% 
+  as_tibble() %>% 
+  filter(str_detect(value, "severe_cmp")) %>% 
+  pull
 
-for (file in filelist) {
-  
-  fp <- paste0("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/pc/", file, sep="")
-  
-  # read in results model
-  load(fp)
-  
-  
-  rmarkdown::render(input = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/model_summary_report.Rmd", 
-                    output_file = paste0("model_", gsub(".rdata","", file), ".html"),
-                    output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports")
-  
-}
+severe_cmp_model = map(severe_cmp, ~results[[.]])
+
+names(severe_cmp_model) = str_remove(severe_cmp, "severe_cmp_")
+
+rmarkdown::render(
+  "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/model_summary_report.Rmd",
+  params = list(data = severe_cmp_model,
+                outcome_name = "Severe Complication (exclude POA)"),
+  output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
+  output_file = "severe_complication.html"
+)
+
+
+
+# death ------
+load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/pc/rating_model.rdata")
+
+death = names(results) %>% 
+  as_tibble() %>% 
+  filter(str_detect(value, "death")) %>% 
+  pull
+
+death_model = map(death, ~results[[.]])
+names(death_model) = str_remove(death, "death_")
+
+rmarkdown::render(
+  "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/model_summary_report.Rmd",
+  params = list(data = death_model,
+                outcome_name = "Death within 30 days"),
+  output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
+  output_file = "death_30d.html"
+)
+
 
