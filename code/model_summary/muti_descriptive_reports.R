d4b1c2777f05665e77515a36a7a411e866478282
Xilin Chen <xilinc@med.umich.edu>
2020-08-04 14:44:45 -0400

    multi descriptive reports

---
+++ code/model_summary/muti_descriptive_reports.R
@@ -0,0 +1,23 @@
+# 12 months in practice
+rmarkdown::render(
+  "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/descriptive_statistics.Rmd",
+  params = list(n_month = 12),
+  output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
+  output_file = "descriptive_12_months.html"
+)
+
+# 24 months in practice
+rmarkdown::render(
+  "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/descriptive_statistics.Rmd",
+  params = list(n_month = 24),
+  output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
+  output_file = "descriptive_24_months.html"
+)
+
+# 36 months in practice
+rmarkdown::render(
+  "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/descriptive_statistics.Rmd",
+  params = list(n_month = 36),
+  output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
+  output_file = "descriptive_no_limit.html"
+)
