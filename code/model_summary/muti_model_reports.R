4a57ecd080280116c529fdb916ec8ee2e863fd7d
Xilin Chen <xilinc@med.umich.edu>
2020-08-06 10:29:12 -0400

    multi cohorts based on practice months, models and visual

--- code/model_summary/muti_model_reports.R
+++ code/model_summary/muti_model_reports.R
@@ -2,7 +2,16 @@ library(knitr)
 library(readr)
 library(tidyverse)
 
-load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/pc/rating_model.rdata")
+# choose how many months of practice after graduation
+n_month = 36
+
+if (n_month == 12) {
+  load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/models_month12_pc.rdata")
+} else if (n_month == 24) {
+  load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/models_month24_pc.rdata")
+} else if (n_month > 24) {
+  load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/models_non_limit_pc.rdata")
+}
 
 # severe complication ------
 severe_cmp = names(results) %>% 
@@ -17,9 +26,10 @@ names(severe_cmp_model) = str_remove(severe_cmp, "severe_cmp_")
 rmarkdown::render(
   "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/model_summary_report.Rmd",
   params = list(data = severe_cmp_model,
+                n_month = n_month,
                 outcome_name = "Severe Complication (exclude POA)"),
   output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
-  output_file = "severe_complication.html"
+  output_file = paste0("severe_complication-", n_month, "_months.html")
 )
 
 
@@ -38,9 +48,10 @@ names(death_model) = str_remove(death, "death_")
 rmarkdown::render(
   "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/code/model_summary/model_summary_report.Rmd",
   params = list(data = death_model,
+                n_month = n_month,
                 outcome_name = "Death within 30 days"),
   output_dir = "/Users/xilinchen/Documents/Repo/Milestone_Medicare_Outcomes/reports",
-  output_file = "death_30d.html"
+  output_file = paste0("death_30days-", n_month, "_months.html")
 )
 
 
--- code/visual/odds_ratio_plot.R
+++ code/visual/odds_ratio_plot.R
@@ -3,17 +3,25 @@ library(glmmTMB)
 library(mixedup)
 library(purrr)
 
-load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/pc/rating_model.rdata")
+n_month = 30
+
+if (n_month == 12) {
+  load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/models_month12_pc.rdata")
+} else if (n_month == 24) {
+  load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/models_month24_pc.rdata")
+} else if (n_month>24) {
+  load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/models_non_limit_pc.rdata")
+}
 
 # overall rating
 mean_rating = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
   filter(term == "IntResponseValue_mean")
 
 less_than_8 = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
-  filter(term == "ever_less_8_rating") # if one rate is less than 7
+  filter(str_detect(term, "mean_ge_8")) # if one rate is less than 8
 
 mean_lt_8_rating = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
-  filter(term == "mean_lt_8")
+  filter(term == "never_less_8_rating")
 
 
 # professional
@@ -21,27 +29,27 @@ prof_rating = map_df(results, extract_fixed_effects, .id = "outcome")  %>%
   filter(term == "prof_rating_mean") 
 
 prof_rating_lt8 = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
-  filter(term == "prof_rating_lt8") 
+  filter(str_detect(term,"prof_rating_ge8"))
 
 # operative
 operative_mean = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
   filter(term == "operative_rating_mean")
 
 operative_mean_lt8 = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
-  filter(term == "operative_rating_lt8")
+  filter(term == "operative_rating_ge8")
+
 
 # leadership
 leadership_rating = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
   filter(term == "leadership_rating_mean")
 
 leadership_rating_lt8 = map_df(results, extract_fixed_effects, .id = "outcome")  %>% 
-  filter(term == "leadership_rating_lt8")
+  filter(term == "leadership_rating_ge8")
 
 
 
-# Odds ratio for all outcomes by predictors for partical colectomy-----------
-
-rbind(
+# Odds ratio for all outcomes by predictors for partial colectomy-----------
+or_table = rbind(
   mean_rating,
   less_than_8,
   mean_lt_8_rating,
@@ -51,15 +59,21 @@ rbind(
   operative_mean_lt8,
   leadership_rating,
   leadership_rating_lt8
-) %>% 
-  mutate(OR = exp(value),
-         OR_lower = exp(lower_2.5),
-         OR_upper = exp(upper_97.5),
-         outcome = str_remove_all(
-           outcome,
-           "Par_|_all_mean|_prof|_lt8|_operative|_leadership|_mean_less_8|_ever_less_8"
-         )
-  ) %>% 
+) %>%
+  mutate(
+    OR = exp(value),
+    OR_lower = exp(lower_2.5),
+    OR_upper = exp(upper_97.5),
+    outcome = str_remove_all(
+      outcome,
+      "Par_|_all_mean|_prof|_lt8|_operative|_leadership|_mean_ge_8|_never_less_8|_ge8"
+    ),
+    term = ifelse(term == "IntResponseValue_mean", "overall_mean", term)
+  ) 
+
+
+# plot
+or_table %>% 
   ggplot(aes(y = OR, x = reorder(outcome, OR))) +
   geom_point() +
   geom_vline(xintercept = 0, color = "firebrick4", alpha = 1/10) +
@@ -70,51 +84,18 @@ rbind(
   # lims(y = c(0.5,2)) +
   coord_flip() +
   facet_wrap( ~ term, ncol=2) +
-  theme(panel.grid   = element_blank(),
-        panel.grid.major.y = element_line(color = alpha("firebrick4", 1/4), linetype = 3),
-        axis.text.y  = element_text(hjust = 0),
-        axis.ticks.y = element_blank()) +
-  labs(x = "Patient Outcomes", caption = "Partial Colectomy") 
-
-ggsave("images/or_bar.png")
-
-
-# odds ratio plot for all procedures, outcomes and milestones -------
-
-load("/Volumes/George_Surgeon_Projects/Milestone_vs_Outcomes/model/all_out_proc_rating.rdata")
-
-primaries = c("IntResponseValue_mean", "prof_rating_mean", "operative_rating_mean",
-              "ever_less_7_rating")
-
-map_df(results, extract_fixed_effects, .id = "outcome") %>% 
-  filter(term %in% primaries) %>% 
-  mutate(OR = exp(value),
-         OR_lower = exp(lower_2.5),
-         OR_upper = exp(upper_97.5)
-  ) %>% 
-  filter(se<1) %>% 
-  ggplot(aes(y = OR, x = reorder(outcome, OR))) +
-  geom_point() +
-  geom_vline(xintercept = 0, color = "firebrick4", alpha = 1/10) +
-  geom_hline(yintercept = 1, alpha = 0.25) +
-  geom_pointrange(aes(ymin = OR_lower,
-                      ymax = OR_upper),
-                  # .width = .95,
-                  size = 3/4, color = "#990024") +
-  lims(y = c(0.5,2)) +
-  coord_flip() +
-  theme(panel.grid   = element_blank(),
-        panel.grid.major.y = element_line(color = alpha("firebrick4", 1/4), linetype = 3),
-        axis.text.y  = element_text(hjust = 0),
-        axis.ticks.y = element_blank()) +
-  xlab("Patient Outcomes") 
-
-ggsave("code/visual/or_all.png")
-ggsave("images/or_all.png")
-
-
-test = rbind(mean_rating, prof_rating, operative_mean, less_than_7) %>% 
-  mutate(OR = exp(value),
-         OR_lower = exp(lower_2.5),
-         OR_upper = exp(upper_97.5)
+  theme(
+    panel.grid   = element_blank(),
+    panel.grid.major.y = element_line(color = alpha("firebrick4", 1 /
+                                                      4), linetype = 3),
+    axis.text.y  = element_text(hjust = 0),
+    axis.ticks.y = element_blank()
+  ) +
+  labs(
+    x = "Patient Outcomes",
+    title = paste0("Odds Ratio Plot for Partial Colectomy - ", n_month, " months")
   )
+
+ggsave(paste0("images/or_bar_", n_month, "months.png"))
+
+
