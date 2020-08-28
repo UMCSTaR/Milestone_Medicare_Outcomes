998fd278722af16b77bf4a10713f94a830c73417
Xilin Chen <xilinc@med.umich.edu>
2020-08-28 16:30:20 -0400

    add by milestone category plots

--- code/visual/or_and_predit_rate.R
+++ code/visual/or_and_predit_rate.R
@@ -44,7 +44,7 @@ mean_rating_tbl %>%
 
 
 # or plot -----
-or = ggplot(data = mean_rating_tbl %>%
+ggplot(data = mean_rating_tbl %>%
          mutate(
            outcome = factor(outcome,
                             levels = c(
@@ -73,7 +73,7 @@ or = ggplot(data = mean_rating_tbl %>%
   facet_wrap(~term, strip.position = "left", ncol = 1) +
   coord_flip() +
   labs(x = "", y = "Odds Ratio (95% CI)",
-       title = "Risk-adjusted Odds Ratios Plot") +
+       title = "Risk-adjusted Odds Ratios") +
   visibly::theme_trueMinimal(center_axis_labels = T) +
   theme(axis.text.y=element_blank(),
         axis.text.x = element_text(size = 10),
@@ -174,11 +174,11 @@ bin_mean_pred_tbl %>%
   geom_pointrange(aes(ymin = conf.low, ymax = conf.high, shape = outcome), alpha = 0.7, position = position_dodge(width = 0.6)) +
   
   # horizontal 
-  # facet_wrap(~ term, nrow = 1, strip.position = "bottom") +
+  facet_wrap(~ term, nrow = 1, strip.position = "bottom") +
   
   # vertical
-  facet_wrap(~ term, nrow = 4, strip.position = "left") +
-  coord_flip() +
+  # facet_wrap(~ term, nrow = 4, strip.position = "left") +
+  # coord_flip() +
   
   labs(title = "Predicted Probabilities of Binay Ratings",
        y = "",
@@ -191,13 +191,15 @@ bin_mean_pred_tbl %>%
   
 ggsave(filename = "images/predicted_prob_prof_cmp_v.png")  # vertical bars
 # ggsave(filename = "images/predicted_prob_prof_cmp_h.png") # horizontal bars
-    
+
+
 # combine plots -----
 library(gridExtra)
 combo = grid.arrange(or + theme(legend.position = "none") + labs(title = "") , 
                      v + theme(legend.position = "none",
                                strip.text.y = element_blank()) + labs(title = "", y = "predicted probability"),
                      ncol = 2)
+
 ggsave(combo, filename = "images/or_prob_combo.png")
 
 # geom_segment arrow-----
@@ -218,7 +220,8 @@ ci_bars =
     position = position_dodge(width = 0.6),
     show.legend = FALSE
   ) +
-  geom_point(color = "gray50", show.legend = FALSE) +
+  # geom_point(color = my_color[[1]], show.legend = FALSE, size = 4) +
+  geom_point(color = my_color[[1]], show.legend = FALSE) +
   scale_y_continuous(labels = scales::percent) 
 
 ci_bars +
@@ -244,7 +247,7 @@ ci_bars +
   theme(legend.title = element_blank()) +
   visibly::theme_trueMinimal(center_axis_labels = T) 
 
-ggsave("images/prob_segment.png")
+ggsave("images/prob_segment_arrow.png")
  
 
 # diff probs ------
@@ -264,4 +267,100 @@ bin_mean_pred_tbl %>%
         legend.title = element_blank()) +
   visibly::theme_trueMinimal(center_axis_labels = T) 
   
+
+# round 2------
+# 1. OR--------
+milestone_terms = unique(mean_rating_tbl$term)
+p_or = paste0("p_or", 1:4)
+
+for (i in seq_along(milestone_terms)) {
+  by_milestone = mean_rating_tbl %>% 
+    filter(term == milestone_terms[[i]]) %>% 
+    mutate(
+      outcome = factor(outcome,
+                       levels = c(
+                         "death", "severe_cmp","any_cmp", "readmit"
+                       )))
+  
+  p = ggplot(data = by_milestone,
+         aes(y = OR, x = outcome)) +
+    geom_hline(
+      yintercept = 1,
+      alpha = 0.25,
+      linetype = 2
+    ) +
+    geom_pointrange(aes(ymin = OR_lower, 
+                        ymax = OR_upper,
+                        color = outcome),
+                    fatten = 5, size = 1, show.legend = F) +
+    scale_color_manual(values = my_color) +
+    labs(x = "", y = "Odds Ratio (95% CI)",
+         title = str_to_title(milestone_terms[[i]])) +
+    visibly::theme_trueMinimal(center_axis_labels = T) +
+    theme(axis.text.x = element_text(size = 10),
+          axis.title.y = element_text(angle = 90, vjust = 0.5, size = 10),
+          axis.ticks.y=element_blank(),
+          strip.text.y = element_text(size = 13),
+          legend.position="bottom",
+          legend.title = element_blank()) 
+  
+  assign(p_or[i], p)
+  
+  # save image
+  ggsave(paste0("images/",milestone_terms[i], "_or.png"))
+  
+}
+
+
+
+# 2. Probability -----
+# side by side bar -----
+milestone_terms = unique(bin_mean_pred_tbl$term)
+p_prob = paste0("p_prob", 1:4)
+
+
+for (i in seq_along(milestone_terms)) {
+ p = bin_mean_pred_tbl %>%
+    filter(term == milestone_terms[i]) %>%
+    
+    ggplot(aes(x = outcome, y = predicted, fill = binary_rating)) +
+    
+    geom_bar(aes(fill = binary_rating), stat = "identity",
+             color="gray50",
+             position = position_dodge(width = 0.55), width = 0.5) +
+    # scale_color_manual(values = my_color) +
+    geom_errorbar(aes(ymin = conf.low, ymax = conf.high), alpha = 0.7, position = position_dodge(width = 0.55), width = .2,show.legend = FALSE) +
+    scale_fill_manual(values = c('firebrick', 'darkgrey'))+
+    
+    labs(title = str_to_title(milestone_terms[i]),
+         y = "",
+         x = "") +
+    scale_y_continuous(labels = scales::percent) +
+    theme(legend.position = c(0.2, 0.9),
+          legend.title = element_blank(),
+    ) +
+    visibly::theme_trueMinimal(center_axis_labels = T) 
+  
+  assign(p_prob[i], p)
+  
+  # save image
+  ggsave(paste0("images/",milestone_terms[i], "_prob.png"))
+  
+}
+
+# 3. combine OR and Prob -----
+
+
+g = grid.arrange(p_or1, p_prob1 ,ncol=2)
+ggsave(file="images/comb_overall_mean.png", g)
+
+g = grid.arrange(p_or2, p_prob2 ,ncol=2)
+ggsave(file = "images/comb_professional.png", g)
+
+g = grid.arrange(p_or3, p_prob3 ,ncol=2)
+ggsave(file = "images/comb_operative.png", g)
+
+g = grid.arrange(p_or4, p_prob4 ,ncol=2)
+ggsave(file = "images/comb_leadership.png", g)
+
 
